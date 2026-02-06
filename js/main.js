7090200afacffa23444e7324a89029765de2ba48
pashechkar-max <polinausakova921@gmail.com>
2026-02-06 11:37:50 +0700

    image commit

---
+++ js/main.js
@@ -0,0 +1,27 @@
+let app = new Vue ({
+    el: '#app',
+    data: {
+        product: "card",
+        allText: "softysofty",
+        categories: ['qwe', 'asd', 'zxc'],
+        variants: [
+            {
+                variantId: 1,
+                variantImage: ".\assets\kusy1.jpg",
+            },
+            {
+                variantId: 2,
+                variantImage: ".\assets\kusy2.jpg",
+            },
+            {
+                variantId: 3,
+                variantImage: ".\assets\kusy3.jpg",
+            },
+            {
+                variantId: 4,
+                variantImage: ".\assets\kusy4.jpg",
+            },
+
+        ]
+    }
+})
