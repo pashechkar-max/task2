c5729fdfee502a64a48936a543c040efe351d5f6
pashechkar-max <polinausakova921@gmail.com>
2026-02-06 12:32:32 +0700

    commit 3

--- js/main.js
+++ js/main.js
@@ -1,62 +1,82 @@
-let app = new Vue ({
-    el: '#app',
+// let app = new Vue ({
+//     el: '#app',
+//
+// })
+// Vue.component('card', {
+//     props: {
+//         truesh: {
+//             type: Boolean,
+//             required: true
+//         }
+//     },
+//     template: `
+// <div class="card">
+//     <h1>{{ title }}</h1>
+//     <div class="card-image">
+//         <img :src="image" :alt="altText"/>
+//     </div>
+//     <div class="card-content">
+//         <p>Description: {{ allText }}</p>
+//         <ul>
+//             <li v-for="categori in categories">{{ categori }}</li>
+//         </ul>
+//     </div>
+// </div>
+//     `,
+//     data(){
+//     return{
+//         product: "card",
+//         allText: "softysofty",
+//         categories: ['qwe', 'asd', 'zxc'],
+//         selectedVariant: 0,
+//         variants: [
+//             {
+//                 variantId: 1,
+//                 variantImage: ".\assets\kusy1.jpg",
+//             },
+//             {
+//                 variantId: 2,
+//                 variantImage: ".\assets\kusy2.jpg",
+//             },
+//             {
+//                 variantId: 3,
+//                 variantImage: ".\assets\kusy3.jpg",
+//             },
+//             {
+//                 variantId: 4,
+//                 variantImage: ".\assets\kusy4.jpg",
+//             },
+//
+//         ]
+//     }
+// },
+//     computed:{
+//         title(){
+//             return this.product;
+//         },
+//         image(){
+//             return this.variants[this.selectedVariant].variantImage;
+//         },
+//
+//     }
+// })
 
-})
-Vue.component('card', {
+
+Vue.component('note-card',{
     props: {
-        truesh: {
-            type: Boolean,
-            required: true
-        }
+        card: Object,
+        locked: Boolean,
+        done: Boolean,
     },
-    template: `
-<div class="card">
-    <h1>{{ title }}</h1>
-    <div class="card-image">
-        <img :src="image" :alt="altText"/>
-    </div>
-    <div class="card-content">
-        <p>Description: {{ allText }}</p>
-        <ul>
-            <li v-for="categori in categories">{{ categori }}</li>
-        </ul>
-    </div>
-</div>
-    `,
-    data(){
-    return{
-        product: "card",
-        allText: "softysofty",
-        categories: ['qwe', 'asd', 'zxc'],
-        selectedVariant: 0,
-        variants: [
-            {
-                variantId: 1,
-                variantImage: ".\assets\kusy1.jpg",
-            },
-            {
-                variantId: 2,
-                variantImage: ".\assets\kusy2.jpg",
-            },
-            {
-                variantId: 3,
-                variantImage: ".\assets\kusy3.jpg",
-            },
-            {
-                variantId: 4,
-                variantImage: ".\assets\kusy4.jpg",
-            },
-
-        ]
+    data: {
+        columns: {
+            todo: [],
+            progress: [],
+            done: []
+        }
     }
-},
-    computed:{
-        title(){
-            return this.product;
-        },
-        image(){
-            return this.variants[this.selectedVariant].variantImage;
-        },
+})
 
-    }
+new Vue({
+    el: '#app',
 })
